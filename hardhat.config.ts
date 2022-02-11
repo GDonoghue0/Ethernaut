7810062dd1ff18b453bb286fe6f3e6fe866a0127
Geoff Donoghue <geoff.donoghue@mail.utoronto.ca>
2022-02-11 15:06:09 -0500

    Able to deploy contracts with hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+// import { HardhatUserConfig, task } from "hardhat/config";
+// import "@typechain/hardhat";
+// import '@openzeppelin/hardhat-upgrades';
+import "@nomiclabs/hardhat-ethers"
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  solidity: "0.6.3",
+};
+
